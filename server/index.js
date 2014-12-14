e4d09fc76802a6321e4bf1954d29fe2171f20940
Adam Barnhard <admbarn@gmail.com>
2014-12-14 11:02:32 -0600

    removed mongoose, only using postgresql

--- server/index.js
+++ server/index.js
@@ -4,18 +4,14 @@ var Hapi           = require('hapi'),
     server         = new Hapi.Server(),
     routes         = require('./routes/config/routes'),
     plugins        = require('./routes/config/plugins'),
-    authentication = require('./routes/config/authentication'),
-    mongoose       = require('mongoose');
+    authentication = require('./routes/config/authentication');
 
 server.connection({address: '0.0.0.0', port: process.env.PORT});
-mongoose.connect(process.env.MONGODB_URL);
 
-mongoose.connection.once('open', function(){
-    server.register(plugins, function(){
-      server.auth.strategy('session', 'cookie', true, authentication);
-      server.route(routes);
-      server.start(function(){
-        server.log('info', server.info.uri);
-      });
-    });
+server.register(plugins, function(){
+  server.auth.strategy('session', 'cookie', true, authentication);
+  server.route(routes);
+  server.start(function(){
+    server.log('info', server.info.uri);
+  });
 });
